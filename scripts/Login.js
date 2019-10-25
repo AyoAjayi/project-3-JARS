5b648add3d3552802037d41b752627d814292e50
Ubuntu <ubuntu@ip-172-31-17-105.ec2.internal>
2019-10-25 03:25:20 +0000

    Centered the sign in with Google button

--- scripts/Login.js
+++ scripts/Login.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import GoogleLogin from 'react-google-login';
+
 /*global gapi*/
 
 // Created a login class to display the Google Login button when page is rendered.
@@ -17,12 +18,14 @@ export class Login extends React.Component {
         console.log(response);
     };
     return (
-      <div>
+      // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css
+      <div className ="wrapper">
         <GoogleLogin
             clientId = '412518169761-vo15jqtbrgcbg30d9t690qc3odu17gk0.apps.googleusercontent.com'
-            buttonText="Login"
+            buttonText="Sign in with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
+            theme='dark'
             cookiePolicy={'single_host_origin'}
         />
       </div>
