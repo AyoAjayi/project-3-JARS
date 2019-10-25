dff0316509a14093a8aa5cbe98803e26ca91592a
Subas Subedi <ssubas999@gmail.com>
2019-10-25 23:58:32 +0000

    Minor changed on app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app = flask.Flask(__name__)
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
+    # return flask.render_template('marketplace.html')  #For testing purpose- to make sure marketplace.html looks okay
     
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
