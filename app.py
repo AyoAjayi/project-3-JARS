cf3086382e1e35258b8bdd9806bfb7ac94d80a74
Ubuntu <ubuntu@ip-172-31-17-105.ec2.internal>
2019-10-26 14:50:47 +0000

    Made sure users can now access the marketplace.html page by modifying the link in index.html

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import os, flask
 app = flask.Flask(__name__)
 @app.route('/')
 def hello():
-    return flask.render_template('index.html')
-    # return flask.render_template('marketplace.html')  #For testing purpose- to make sure marketplace.html looks okay
+    return flask.render_template(('index.html', 'marketplace.html'))
+
+@app.route('/marketplace')
+    
+def market_place():
+    return flask.render_template('marketplace.html')  #For testing purpose- to make sure marketplace.html looks okay
     
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
