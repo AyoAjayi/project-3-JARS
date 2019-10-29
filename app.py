2d5b916c695d8645a66400224085405a2a5421db
Subas Subedi <ssubas999@gmail.com>
2019-10-29 04:27:55 +0000

    Minor changes

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import os, flask
 app = flask.Flask(__name__)
 @app.route('/')
 def hello():
-    return flask.render_template(('index.html', 'marketplace.html'))
+    return flask.render_template('index.html')
+    # return flask.render_template('marketplace.html')
+    # return flask.render_template('submit.html')
+    
 
 @app.route('/marketplace')
-    
 def market_place():
-    return flask.render_template('marketplace.html')  #For testing purpose- to make sure marketplace.html looks okay
+    return flask.render_template('marketplace.html')
+    
+@app.route('/submit')
+def submit():
+    return flask.render_template('submit.html')
     
 app.run(
     host=os.getenv('IP', '0.0.0.0'),
