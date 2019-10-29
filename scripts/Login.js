df28df71218e35e397c356cfff25da23a4896e0e
Ubuntu <ubuntu@ip-172-31-17-105.ec2.internal>
2019-10-29 21:46:53 +0000

    Trying to push to heroku

--- scripts/Login.js
+++ scripts/Login.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import GoogleLogin from 'react-google-login';
 
+
 /*global gapi*/
 
 // Created a login class to display the Google Login button when page is rendered.
@@ -18,7 +19,7 @@ export class Login extends React.Component {
         console.log(response);
     };
     return (
-      // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css
+      // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css file
       <div className ="wrapper">
         <GoogleLogin
             clientId = '412518169761-vo15jqtbrgcbg30d9t690qc3odu17gk0.apps.googleusercontent.com'
