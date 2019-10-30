cb02f3d5d7b8f3b330eb9fcdc205909b43337ab9
Subas Subedi <ssubas999@gmail.com>
2019-10-30 19:48:43 +0000

    Minor fix

--- scripts/Login.js
+++ scripts/Login.js
@@ -33,7 +33,7 @@ export class Login extends React.Component {
       // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css file
       <div className ="wrapper">
         <GoogleLogin
-            clientId = '433826711359-r31ipjdt01vfjhgbdi1go9b1508c7t8g.apps.googleusercontent.com'
+            clientId = '641650714654-3nvhsfpcnhgiljvfrhj70f7idk3uv0gi.apps.googleusercontent.com'
             buttonText="Sign in with Google"
             onSuccess={responseGoogle}
             onFailure={responseGoogle}
