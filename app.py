94ff3d8b962b0db6a247b4b63953ad24f6a3f1ad
Subas Subedi <ssubas999@gmail.com>
2019-11-09 04:03:01 +0000

    Serverside ready for socketio connection

--- app.py
+++ app.py
@@ -1,23 +1,42 @@
-import os, flask
+import os, flask, flask_socketio, flask_sqlalchemy
 
 app = flask.Flask(__name__)
+socketio = flask_socketio.SocketIO(app)
+
 @app.route('/')
 def hello():
     return flask.render_template('index.html')
-    # return flask.render_template('marketplace.html')
-    # return flask.render_template('submit.html')
-    
 
-@app.route('/marketplace')
-def market_place():
-    return flask.render_template('marketplace.html')
+
+# ***** Body *****
+
+@socketio.on('connect')
+def on_connect():
+    print('Someone connected!')
+
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Someone disconnected!')
+
+# *** Server received a new search event sent by client(Nav.js) ***
+@socketio.on('new search')
+def on_new_search(search_data):
+    print ("Got an event for new search with data: "+ str(search_data))
     
-@app.route('/submit')
-def submit():
-    return flask.render_template('submit.html')
     
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
+# *** Server received a new submit event sent by client(Submit.js) ***
+@socketio.on('new submit')
+def on_new_submit(submit_data):
+    print ("Got an event for new submit with data: "+ str(submit_data))
+
+
+# ***** Footer *****
+
+if __name__ =='__main__':
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=int(os.getenv('PORT', 8080)),
+        debug=True
     )
