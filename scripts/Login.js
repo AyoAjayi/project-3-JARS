36cb2a724303bb3493d61359dbf586a8eb2d6630
Subas Subedi <ssubas999@gmail.com>
2019-11-19 14:57:55 +0000

    App automatically redirects to marketplace component on successful login

--- scripts/Login.js
+++ scripts/Login.js
@@ -20,62 +20,63 @@ export class Login extends React.Component {
     };
   }
   
-  componentDidMount(){
-    Socket.on('username_received', (data_one) => {
-            this.setState({
-                'username': data_one['user_name'],
-            });
-        });
-  }
+  // componentDidMount(){
+  //   Socket.on('username_received', (data_one) => {
+  //           this.setState({
+  //               'username': data_one['user_name'],
+  //           });
+  //       });
+  // }
   
-  componentDidMount(){
-    Socket.on('email_received', (data_two) => {
-            this.setState({
-                'email': data_two['user_email'],
-            });
-        });
-  }
-  componentDidMount(){
-    Socket.on('image_received', (data) => {
-            this.setState({
-                'image': data['imageUrl'],
-            });
-        });
-  }
+  // componentDidMount(){
+  //   Socket.on('email_received', (data_two) => {
+  //           this.setState({
+  //               'email': data_two['user_email'],
+  //           });
+  //       });
+  // }
+
+  // componentDidMount(){
+  //   Socket.on('image_received', (data) => {
+  //           this.setState({
+  //               'image': data['imageUrl'],
+  //           });
+  //       });
+  // }
   
   render() {
-    console.log(this.state.username);
-    console.log(this.state.email);
-    console.log(this.state.image);
+    // console.log(this.state.username);
+    // console.log(this.state.email);
+    // console.log(this.state.image);
     
     const responseGoogle = (response) => {
         let auth = gapi.auth2.getAuthInstance();
         let user = auth.currentUser.get();
         
-        // On success, will redirect to marketplace component
         if (user.isSignedIn()) {
             console.log("Google Token:  " + user.getAuthResponse().id_token);
             console.log(response);
-        this.setState({user_credentials: response});
-        Socket.emit('login', 
-            {'data': this.state.user_credentials
-        },
-        );
-            // if (this.state.loggedIn === true) {
-            //   console.log('Inside the function');
-            //   return <Redirect to='/marketplace' />;
-            // }
+            // This will set 'loggedIn' to true
+            this.setState({loggedIn: true});
         }
-       
     };
+    
+    
+    // On success, will redirect to marketplace component
+    if (this.state.loggedIn === true){
+      console.log("Inside the function");
+      return <Redirect to='/marketplace' />;
+    }
+    else {
+      console.log("Not logged in!")
+    }
 
     return (
-  
       // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css file
       <div className = "wrapper">
         <GoogleLogin
           clientId = '433826711359-r31ipjdt01vfjhgbdi1go9b1508c7t8g.apps.googleusercontent.com'
-         usernameonText="Sign in with Google"
+          usernameonText="Sign in with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
           theme='dark'
@@ -85,173 +86,3 @@ export class Login extends React.Component {
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-// import * as React from 'react';
-// import GoogleLogin from 'react-google-login';
-// import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-// import { Marketplace } from './Marketplace';
-// import { Socket } from './Socket';
-// import ReactDOM from 'react-dom';
-
-// /*global gapi*/
-
-
-// // Created a login class to display the Google Login button when page is rendered
-
-
-// export class Login extends React.Component {
-//   constructor(props) {
-//     super(props);
-    
-//     this.state = {
-//       loggedIn : false
-//     };
-//   }
-  
-
-//   render() {
-//     const responseGoogle = (response) => {
-//         let auth = gapi.auth2.getAuthInstance();
-//         let user = auth.currentUser.get();
-        
-//         // On success, will redirect to marketplace component
-//         if (user.isSignedIn()) {
-//             console.log("Google Token:  " + user.getAuthResponse().id_token);
-//             console.log(response);
-//             this.setState({
-//               isLoggedIn: true
-//             });
-//       const renderRedirect = () => {
-//         if (this.state.isLoggedIn) {
-//           return <Redirect to='/marketplace' />;
-//       }
-//       }
-//     }
-//     return (
-//       // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css file
-//       <div className = "wrapper">
-//         <GoogleLogin
-//           clientId = '433826711359-r31ipjdt01vfjhgbdi1go9b1508c7t8g.apps.googleusercontent.com'
-//           usernameonText="Sign in with Google"
-//           onSuccess={responseGoogle}
-//           onFailure={responseGoogle}
-//           theme='dark'
-//           cookiePolicy={'single_host_origin'}
-//         />
-//       </div>
-//     );
-//   };
-// }
-// }
-
-
-// import * as React from 'react';
-// import GoogleLogin from 'react-google-login';
-// import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
-// import { Marketplace } from './Marketplace';
-// import { Socket } from './Socket';
-// import ReactDOM from 'react-dom';
-
-// /*global gapi*/
-
-
-// // Created a login class to display the Google Login button when page is rendered
-
-
-// export class Login extends React.Component {
-//   constructor(props) {
-//     super(props);
-    
-//     this.state = {
-//       loggedIn : false
-//     };
-    
-//     // this.setLoggedIn = () => {
-//     //   this.setState({loggedIn: true});
-//     // };
-//   }
-  
-//   // componentDidMount(){
-//   //   Socket.on('username_received', (data_one) => {
-//   //           this.setState({
-//   //               'username': data_one['user_name'],
-//   //           });
-//   //       });
-//   // }
-  
-//   // componentDidMount(){
-//   //   Socket.on('email_received', (data_two) => {
-//   //           this.setState({
-//   //               'email': data_two['user_email'],
-//   //           });
-//   //       });
-//   // }
-//   // componentDidMount(){
-//   //   Socket.on('image_received', (data) => {
-//   //           this.setState({
-//   //               'image': data['imageUrl'],
-//   //           });
-//   //       });
-//   // }
-  
-//   render() {
-//     const responseGoogle = (response) => {
-//         let auth = gapi.auth2.getAuthInstance();
-//         let user = auth.currentUser.get();
-        
-//         // On success, will redirect to marketplace component
-//         if (user.isSignedIn()) {
-//             console.log("Google Token:  " + user.getAuthResponse().id_token);
-//             console.log(response);
-//             this.setState({
-//               isLoggedIn: true
-//             });
-        
-
-//         // if (this.state.isLoggedIn){
-//         //   console.log(true);
-//         //   return 'AYO';
-//         //   // return <Redirect to='/marketplace' />;
-//         // }
-            
-//         this.setState({user_credentials: response});
-//         Socket.emit('login', 
-//             {'data': this.state.user_credentials
-//         },);
-//             // if (this.state.loggedIn === true) {
-//             //   console.log('Inside the function');
-//             //   return <Redirect to='/marketplace' />;
-//             // }
-//         }
-//       const renderRedirect = () => {
-//       if (this.state.isLoggedIn) {
-//           return <Redirect to='/marketplace' />;
-//     }
-//     };
-
-//     return (
-//       // Added a class for the login component to center it to the middle of the webpage. Css for wrapper class is in style.css file
-//       <div className = "wrapper">
-//         {renderRedirect}
-//         <GoogleLogin
-//           clientId = '433826711359-r31ipjdt01vfjhgbdi1go9b1508c7t8g.apps.googleusercontent.com'
-//           usernameonText="Sign in with Google"
-//           onSuccess={responseGoogle}
-//           onFailure={responseGoogle}
-//           theme='dark'
-//           cookiePolicy={'single_host_origin'}
-//         />
-//       </div>
-//     );
-//   };
-// }
-// }
